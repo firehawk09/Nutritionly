77f0d3c1b6d3c937c53a64f044155238848abf01
Rafael.Quijada <ralphybook@gmail.com>
2015-03-02 15:14:30 -0600

    update1

--- js/NutriSearch.js
+++ js/NutriSearch.js
@@ -13,15 +13,22 @@
         routes: {
             '*default': 'home'
         },
-        home: function() {
+        var YOUR_APP_ID = "944703ea",
+        var YOUR_APP_KEY = "41f92b5b38c4d23259b61a97139c1c35",
 
+        var nutritionix = require('nutritionix')({
+                appId: 'YOUR_APP_ID',
+                appKey: 'YOUR_APP_KEY'
+            }, false);
+
+        home: function() {
         }
     });
 
     Backbone.NutriModel = Backbone.Model.extend({
 
         defaults: {
-            url: ' '
+            url: 'https://api.nutritionix.com/v1_1'
         },
         initialize: function() {
         }
