99d94df8e0e06fe3d6f07215b9c80e40fea43e0f
Rafael.Quijada <ralphybook@gmail.com>
2015-03-03 11:06:25 -0600

    hey phil

--- js/NutriSearch.js
+++ js/NutriSearch.js
@@ -9,21 +9,38 @@
         routes: {
             '*default': 'home'
         },
-        var YOUR_APP_ID = "944703ea",
-        var YOUR_APP_KEY = "41f92b5b38c4d23259b61a97139c1c35",
+        YOUR_APP_ID: function() {
+            return "944703ea"
+        },
+
+        YOUR_APP_KEY: function() {
+            return "41f92b5b38c4d23259b61a97139c1c35"
+        },
 
         var nutritionix = require('nutritionix')({
-                appId: 'YOUR_APP_ID',
-                appKey: 'YOUR_APP_KEY'
-            }, false);
+            appId: 'YOUR_APP_ID',
+            appKey: 'YOUR_APP_KEY'
+        }, false);
 
         home: function() {
+            this.model = new Backbone.Song
+            this.homeView = z(Backbone.SongView, {
+                model: this.model
+            });
+            var self = this;
+            this.model.fetch().then(function(d) {
+            })
 
         },
         initialize: function() {
+            console.log(yey);
             this.el = document.querySelector('.container');
             Backbone.history.start();
         }
+    });
+
+    Backbone.NutriScience = Backbone.Collection.extend({
+        model: Backbone.NutriModel
     });
 
     Backbone.NutriModel = Backbone.Model.extend({
@@ -32,6 +49,7 @@
             url: 'https://api.nutritionix.com/v1_1'
         },
         initialize: function() {
+            console.log(yey);
         }
     });
 
--- js/app.js
+++ js/app.js
@@ -22,24 +22,24 @@ function app(){
         // {url: "./bower_components/parse-js-sdk/lib/parse.min.js"},
 
         // when using React (and the plugin JSnoX), uncomment the following two lines
-        {url: "./bower_components/react/react.min.js"},
-        {url: "./bower_components/jsnox/index.js"},
+        // {url: "./bower_components/react/react.min.js"},
+        // {url: "./bower_components/jsnox/index.js"},
 
         // other stuff
         {url: "./bower_components/pace/pace.min.js"},
         {url: "./js/TemplateView.js"},
-        {url: "./js/leafLet.js"},
-        {url: "./js/NutriSearch.js"}
+        // {url: "./js/NutriSearch.js"},
+        {url: "./js/leafLet.js"}
     ).then(function(){
         // if turning on JSnoX, uncommment the following line
-        window.z = jsnox(React);
+        // window.z = jsnox(React);
         // if turning on React, uncomment the following line
-        React.initializeTouchEvents(true);
+        // React.initializeTouchEvents(true);
 
         document.querySelector("html").style.opacity = 1;
         // start app?
 
-        new Backbone.NutriRouter();
+        // new Backbone.NutriRouter();
     })
 
 }
