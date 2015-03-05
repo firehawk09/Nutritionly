7113d33ca4e38b2f064ecc1d5ce4a312f87e979f
Rafael.Quijada <ralphybook@gmail.com>
2015-03-05 15:53:53 -0600

    V0.6.4.1

--- js/NutriSearch.js
+++ js/NutriSearch.js
@@ -1,39 +1,59 @@
 ;
 (function(exports) {
 
-    "use strict";
+        "use strict";
 
-    // Router
+        // Router
 
-    Backbone.NutriRouter = Backbone.Router.extend({
-        initialize: function() {
-            console.log("initialized");
-            this.collection = new Backbone.NutriScience();
-            this.collection.zip = 77365
-            this.view = new Backbone.HealthyView({
-                collection: this.collection
-            });
+        Backbone.NutriRouter = Backbone.Router.extend({
+                initialize: function() {
+                    console.log("initialized");
+                    this.collection = new Backbone.NutriScience();
+                    this.collection.zip = 77365
+                    this.view = new Backbone.HealthyView({
+                        collection: this.collection
+                    });
 
-            this.container = document.querySelector('.leftContainer');
+                    this.container = document.querySelector('.leftContainer');
 
-            Backbone.history.start();
-        },
+                    Backbone.history.start();
+                },
 
-        routes: {
-            '': 'home',
-            'details/*': 'details'
-        },
+                routes: {
+                    '': 'home',
+                    '/logIn': 'logIn',
+                    '/#id/:*': 'details',
+                    "search/:query": "search"
+                },
 
-        home: function(){
-            this.collection.fetch()
-        }
-    });
+                home: function() {
+                    this.collection.fetch()
+                }
+
+                logIn: function(){
+                    this.collection.create()
+                },
+
+                // details: function(){
+
+                // },
+
+                // search: function(keywords) {
+                //     this.draw(keywords);
+                // }
+        });
 
     // Views
 
     Backbone.HealthyView = Backbone.TemplateView.extend({
         el: ".leftContainer",
-        view: "listTemp"
+        view: "listTemp",
+        events: {
+            "submit #submitForm": "submit"
+        },
+        submit: function(e){
+                    this.collection.create();
+        }
     });
 
     // Models
@@ -42,10 +62,10 @@
         defaults: {
             zip: 77365
         },
-        url: function(){
+        url: function() {
             return [
                 "https://nutritions.herokuapp.com/api/v1/venues",
-                this.get('zip') ? '?near='+this.get('zip') : ''
+                this.get('zip') ? '?near=' + this.get('zip') : ''
             ].join('')
         }
     });
@@ -54,13 +74,13 @@
 
     Backbone.NutriScience = Backbone.Collection.extend({
         model: Backbone.NutriModel,
-        url: function(){
+        url: function() {
             return [
                 "https://nutritions.herokuapp.com/api/v1/venues",
-                this.zip ? '?near='+this.zip : ''
+                this.zip ? '?near=' + this.zip : ''
             ].join('')
         },
-        parse: function(data){
+        parse: function(data) {
             return data.venues
         }
     });
